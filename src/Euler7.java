3e2f5feccd061509768d8f05bbf188ece9d1b64b
Arjun Vijay <arjunvijay@gwu.edu>
2018-07-30 23:44:46 -0400

    Brute Force solution to Problem #7

---
+++ src/Euler7.java
@@ -0,0 +1,48 @@
+public class Euler7{
+	public static void main(String[] args){
+		int nth_prime_number = 2; //keeps track of the actual numerical value of the prime number
+		int counter = 1; //keeps track of how many prime number we have figured out
+		int number = 2;
+
+		while(counter < 10002) {
+			if(isPrime(number) == true) {
+				nth_prime_number = number;
+				//System.out.println("The " + counter + " prime number is " + nth_prime_number);
+				counter++;
+				number++;
+			} else {
+				number++;
+			}
+
+		}
+
+		System.out.println("The 10,001 prime number is " + nth_prime_number);
+
+	}
+
+	public static boolean isPrime(int number) {
+		int counter = 2;
+		while (counter <= number) {
+			int remainder = number % counter;
+			if(number == counter) {
+				return true;
+			} else if(remainder == 0) {
+				return false;
+			} else {
+				counter++;
+			}
+		}
+
+		return false;
+	}
+
+}
+
+
+
+
+//find the 10,001 prime number
+//find prime number by using a counter
+//establish a number as prime if it does not divisble by any number smaller than it
+	//given a number n
+	//check to see if its prime
